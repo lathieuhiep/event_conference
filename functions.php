38cd3d04a870e259fa033f2b7503b54ac50d3187
lathieuhiep <khacdiepkma90@gmail.com>
2018-08-01 21:16:55 +0700

    remove woocommerce

--- functions.php
+++ functions.php
@@ -488,13 +488,6 @@ function event_conference_register_required_plugins() {
             'required'  =>  true,
         ),
 
-        // This is an example of how to include a plugin from the WordPress Plugin Repository
-        array(
-            'name'      =>  'Woocommerce',
-            'slug'      =>  'woocommerce',
-            'required'  =>  true,
-        ),
-
     );
 
     /**
