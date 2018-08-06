882e94ac91e40f978549d9fed1991d1edac858d4
lathieuhiep <khacdiepkma90@gmail.com>
2018-08-06 20:35:30 +0700

    rename

---
+++ template-parts/breadcrumb/inc-breadcrumb.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Breadcrumb file
+ */
+
+if ( function_exists( 'bcn_display' ) ) :
+?>
+
+<div class="event-breadcrumb">
+    <div class="container">
+        <div class="row">
+            <div class="breadcrumb-content col-md-12">
+                <div class="breadcrumb-wrapper">
+                    <?php bcn_display(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+endif;
