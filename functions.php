f2e44acc9785ce22658c45f77d2995b6bd38df19
lathieuhiep <khacdiepkma90@gmail.com>
2018-08-28 23:12:03 +0700

    update check  load css

--- functions.php
+++ functions.php
@@ -794,3 +794,39 @@ function event_conference_social_network_share() {
 <?php
 }
 /* End social network share */
+
+function remove_jquery_migrate( $scripts ) {
+    if ( ! is_admin() && isset( $scripts->registered['jquery'] ) ) {
+        $script = $scripts->registered['jquery'];
+        if ( $script->deps ) {
+            $script->deps = array_diff( $script->deps, array( 'jquery-migrate' ) );
+        }
+    }
+}
+add_action( 'wp_default_scripts', 'remove_jquery_migrate' );
+
+add_action('wp_print_styles', 'my_deregister_styles', 100);
+
+function my_deregister_styles() {
+    global $post;
+
+
+    wp_deregister_style('font-awesome');
+    wp_deregister_style('wpcr_font-awesome');
+
+    if ( !is_singular( 'post' ) ) :
+        wp_deregister_style('wpcr_style');
+    endif;
+
+    if ( ! empty( $post ) && is_a( $post, 'WP_Post' ) ) :
+        $plugin_photo = $post->post_content;
+
+        if ( !has_shortcode( $plugin_photo, 'contact-form-7' ) ) :
+
+            wp_deregister_style( 'contact-form-7' );
+
+        endif;
+
+    endif;
+
+}
